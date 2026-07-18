89580c4692f34eae6d622abbb087740d51be7f6b
Abdelrhman Elbrens <adotelbrens@gmail.com>
2026-07-18 16:29:43 +0300

    Updated migrations

---
+++ DataAccessLayer/Migrations/20260706123016_UpdateTaxAndNationalID.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTaxAndNationalID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NationalID",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BussinessTaxID",
+                table: "Owner",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NationalID",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "BussinessTaxID",
+                table: "Owner");
+        }
+    }
+}
--- DataAccessLayer/obj/Debug/net10.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net10.0/DataAccessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7907e28096e0980077415634c4c1600cc2cfdfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b545e8b2f9d310e902ae8d00089e9ffbc23bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
